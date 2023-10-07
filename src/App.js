b233bbfb5ece7d3154894e9edad2619784345822
CuoDan <cuong1.dang7@gmail.com>
2023-10-07 21:44:39 +0000

    Third Load

--- src/App.js
+++ src/App.js
@@ -4,8 +4,11 @@ import {App2} from "./App2";
 export function App() {
     return (
         <>
-            <img src={moonFestivalImage} className='w-72' />
-            <h1 className="text-amber-600">Hello World Again!</h1>
+            <br></br>
+            <img src={moonFestivalImage} className='w-72 justify-center' />
+            <br></br>
+            <h1 className="text-amber-600 text-center">Hello World Again!</h1>
+            <br></br>
             <App2></App2>
         </>
     );
--- src/App2.js
+++ src/App2.js
@@ -3,8 +3,9 @@ import myImage from './images/myImage.webp'
 export function App2() {
     return (
         <>
-            <img src={myImage} className='w-72' /> 
-            <h1 className="text-amber-600">I dunno what to say</h1>
+            <img src={myImage} className='w-72 text-center'/>
+            <br></br>
+            <h1 className="text-amber-600 text-center">I dunno what to say</h1>
         </>
     );
 }
