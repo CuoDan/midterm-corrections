fb16aa201ac85a8ddb59c34b1df977215799322b
CuoDan <cuong1.dang7@gmail.com>
2023-10-22 20:53:21 +0000

    3rd Load

--- src/9-Tile.test.js
+++ src/9-Tile.test.js
@@ -25,7 +25,7 @@ it("should display image when image value is provided", () => {
     expect(imageElement).toBeTruthy();
 });
 
-//Option 5
+//Option 5 (I think option 5 is wrong...)
 it("should NOT display image when image value is missing", () => {
     //Arange
     const content = [{ title: "test", image: ""}];
@@ -34,6 +34,7 @@ it("should NOT display image when image value is missing", () => {
     const { queryByRole } = render(<Tile content={content} />);
 
     //Assert
-    const imageElement = queryByRole("img");
-    expect(imageElement).toBeFalsy();
+    //I think this is wrong because even if {image: ""}, there would still be a <img /> in Tile.js (the only thing that changes is that <img /> would have no source) 
+    const imageElement = queryByRole("img"); // so when you queryByRole("img"), it would still be true, but there just won't be any source so the image wouldn't display
+    expect(imageElement).toBeFalsy(); //Show error "expect(received).toBeFalsy() /n Received: <img />"
 });
